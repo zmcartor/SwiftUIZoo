4f27d2692491b797a5c943ecb90b48226b418c11
Zach McArtor <zmcartor@gmail.com>
2021-02-22 15:14:04 -0700

    Initial Commit

---
+++ SwiftUIZoo/SwiftUIZooApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIZooApp.swift
+//  SwiftUIZoo
+//
+//  Created by zm on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIZooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
