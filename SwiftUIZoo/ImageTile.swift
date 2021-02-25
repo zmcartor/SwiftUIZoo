3202ab1e113389e3f7874c9dafc38b4ede8839be
Zach McArtor <zmcartor@gmail.com>
2021-02-24 21:53:26 -0700

    strega image tile zoo

--- SwiftUIZoo/ImageTile.swift
+++ SwiftUIZoo/ImageTile.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct ImageTile: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            Text("Strega")
+                .font(.headline)
+                .fontWeight(.black)
+                .kerning(20)
+            
+            Image("strega")
+                
+            Divider()
+            
+            Text("Alchemy")
+                .font(.callout)
+                .fontWeight(.ultraLight)
+                .kerning(30)
+    
+            Image("strega")
+                // stretch the litteral hell outta it
+                .resizable(capInsets: EdgeInsets(top: 3, leading: 4, bottom: 114, trailing: 5), resizingMode: .stretch)
+            Divider()
+            Image("strega")
+                // tile and fill the entire space
+                .resizable(capInsets: EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0), resizingMode: .tile)
+            
+        }
     }
 }
 
